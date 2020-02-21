e81aa2c32e10251fa7d227547fdc9df79d4e871a
Xristos Diamantis <xristos567@gmail.com>
2020-02-21 15:39:00 +0200

    Update test_todo_api.py

--- test_todo_api.py
+++ test_todo_api.py
@@ -18,9 +18,12 @@ def print_resp(resp):
         print(json.dumps(resp.json(), indent=2))
           
 
-def test_signup(name,email,password,password_confirmation):
-    data = {"name":name,"email":email,"password":password,"password_confirmation":password_confirmation}
-    resp = requests.post(url+"signup",data)
+def signup():
+    data = {"name":User.name,"email":User.email,"password":User.password,"password_confirmation":User.password}
+    print("Try to sign up...")
+    resp = requests.post(wurl("signup"),data)
+    print("response:\n")
+    print_resp(resp)
 
 
 print("Try to login...")
@@ -42,6 +45,7 @@ User.token=resp.json()["auth_token"]
 resp = requests.post(wurl("todos"),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"},params={"title":"my first post"})
 print("response:\n")
 print_resp(resp)
+todo_id=resp.json()["id"]
 input('\nEnter to continue....\n\n')
 
 print("Try to get first page of todos..")
@@ -50,46 +54,48 @@ print("response:\n")
 print_resp(resp)
 input('\nEnter to continue....\n\n')
 
-print("Try to get todo with id=1...")
-resp = requests.get(wurl("todos/1"),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"})
+print("Try to get todo with id="+str(todo_id)+"...")
+resp = requests.get(wurl("todos/"+str(todo_id)),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"})
 print("response:\n")
 print_resp(resp)
 input('\nEnter to continue....\n\n')
 
-print("Try to change todo with id=1...")
-resp = requests.put(wurl("todos/1"),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"},params={"title":"Changed title"})
+print("Try to change todo with id="+str(todo_id)+"...")
+resp = requests.put(wurl("todos/"+str(todo_id)),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"},params={"title":"Changed title"})
 print("response:\n")
 print_resp(resp)
 input('\nEnter to continue....\n\n')
 
-print("Try to delete todo with id=1...")
-resp = requests.delete(wurl("todos/1"),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"})
+print("Try to delete todo with id="+str(todo_id)+"...")
+resp = requests.delete(wurl("todos/"+str(todo_id)),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"})
 print("response:\n")
 print_resp(resp)
 input('\nEnter to continue....\n\n')
 
-print("Try to create item for todo with id=2...")
-resp = requests.post(wurl("todos/2/items"),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"},params={"name":"my first comment"})
+resp = requests.post(wurl("todos"),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"},params={"title":"my first post"})
+todo_id2=resp.json()["id"]
+print("Try to create item for todo with id="+str(todo_id2)+"...")
+resp = requests.post(wurl("todos/"+str(todo_id2)+"/items"),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"},params={"name":"my first comment"})
 print("response:\n")
 print_resp(resp)
 id=resp.json()["items"].pop()["id"]
 print("\nitem created with id="+str(resp.json()["items"].pop()["id"]))
 input('\nEnter to continue....\n\n')
 
-print("Try to get items for todo with id=2...")
-resp = requests.get(wurl("todos/2/items"),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"})
+print("Try to get items for todo with id="+str(todo_id2)+"...")
+resp = requests.get(wurl("todos/"+str(todo_id2)+"/items"),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"})
 print("response:\n")
 print_resp(resp)
 input('\nEnter to continue....\n\n')
 
-print("Try to change item with id="+str(id)+" and todo with id=2...")
-resp = requests.put(wurl("todos/2/items/"+str(id)),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"},params={"name":"Changed name"})
+print("Try to change item with id="+str(id)+" and todo with id="+str(todo_id2)+"...")
+resp = requests.put(wurl("todos/"+str(todo_id2)+"/items/"+str(id)),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"},params={"name":"Changed name"})
 print("response:\n")
 print_resp(resp)
 input('\nEnter to continue....\n\n')
 
-print("Try to delete item with id="+str(id)+" and todo with id=2...")
-resp = requests.delete(wurl("todos/2/items/"+str(id)),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"})
+print("Try to delete item with id="+str(id)+" and todo with id="+str(todo_id2)+"...")
+resp = requests.delete(wurl("todos/"+str(todo_id2)+"/items/"+str(id)),headers={"Authorization":User.token,"Accept":"'application/vnd.todos.v1+json'"})
 print("response:\n")
 print_resp(resp)
 input('\nEnter to continue....\n\n')
